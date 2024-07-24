bfaf772a59dceacb1fa0eb6161a8bac930f8be11
Matheus <matheusmunhoz35@gmail.com>
2024-07-23 21:19:51 -0300

    mudificações pre-delete

---
+++ testesAPI/apicriarconta.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+criar_cont_api = 'https://apipf.jogajuntoinstituto.org/register'
+email = 'matheusmu303@gmail.com'
+
+cria_cont = {
+    "email": email,
+    "password": 'AAAAb'
+}
+
+
+ordem_cria_cont = sorted(cria_cont.items(),key=lambda x:['email','password'].index(x[0]))
+ordem_cria_cont = dict(ordem_cria_cont)
+
+response = requests.post(criar_cont_api,json=ordem_cria_cont)
+
+if response.status_code == 200:
+    print("sucesso!!!!!!!!!!!!!!!!!!!!!!")
+    cria_cont_response = response.json()
+    with open('cria_cont_response.json','w') as json_file:
+        json.dump(cria_cont_response, json_file, indent=4)
+
+    print("json feito com sucesso")
+else:
+    print(f"deu errado pois{response.text}")
+
---
+++ testesAPI/apilogin.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+
+email = 'matheusmu303@gmail.com'
+post_login = 'https://apipf.jogajuntoinstituto.org/login'
+
+login_data = {
+    "email": email,
+    "password": 'AAAAb'
+    }
+
+ordem_login = sorted(login_data.items(),key=lambda x:['email','password'].index(x[0]))
+ordem_login = dict(ordem_login)
+
+response = requests.post(post_login,json=ordem_login)
+if response.status_code == 200:
+    login_response = response.json()
+    with open('login_response.json', 'w') as json_file:
+        json.dump(login_response,json_file,indent=4)
+        print("Resposta do login salva em login_response.json")
+else:
+    print(f"Erro ao fazer login: {response.text}")
+
